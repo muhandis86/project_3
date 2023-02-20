3e72de91b84bc192d0284b53118bbab4d92a0293
Zaur <eskendarov.gmail.com>
2023-02-20 09:24:59 +0300

    comlete layot for all sections

--- src/js/script.js
+++ src/js/script.js
@@ -30,4 +30,91 @@ $(document).ready(function(){
         }
       ]
   });
+
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  // Modal windows
+  $('[data-modal=consultation]').on('click', function() {
+   $('.overlay, #consultation').fadeIn('fast');
+  });
+
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+  });
+
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('fast');
+    });
+  });
+
+  $(window).on('click', function(e) {
+    if (e.target.classList.contains('overlay')) {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+    }
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {   // esc
+       $('.overlay, #consultation, #thanks, #order').fadeOut('fast');
+    }
+  });
+
+  // $('#consultation-form').validate();
+  // $('#consultation form').validate();
+  // $('#order form').validate();
+
+  function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 3
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Введите имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+            phone: "Введите номер телефона",
+            email: {
+              required: "Введите e-mail",
+              email: "Неправильно введен e-mail"
+            }
+        }
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
+
 });
+
+
